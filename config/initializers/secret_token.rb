eb261c9dbfcae520007a14e8950add040d405b47
Blake Rego <blake.rego@gmail.com>
2013-12-07 16:26:47 -0500

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SparkleWeb::Application.config.secret_key_base = '0d2cda95a09fe2665ea074f2265fa5543bb484f9049d58a11781204ccea4dc9fd11eabc159ca1cf1c77035ccb4092ec4a2ef25e204151d09269fdd2b8c9b4c67'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SparkleWeb::Application.config.session_store :cookie_store, key: '_sparkle_web_session'
